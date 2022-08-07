04dcd30f1ca21c01b9809316edc73c27439fcbdd
Dexmi8000 <104418971+Dexmi8000@users.noreply.github.com>
2022-08-07 21:42:48 +0300

    Delete src directory

--- src/Link.js
+++
@@ -1,9 +0,0 @@
-let Link = () => {
-    return(
-        <a className="App-link" href="https://reactjs.org">
-        My Dex-project - Best of the Best
-        </a>
-    )
-}
-
-export default Link;
--- src/My_text.js
+++
@@ -1,9 +0,0 @@
-const My_text = () => {
-    return(
-        <p className='Prrr'>
-            Its magiс!!!
-        </p>
-    );
-  }
-
-export default My_text;
